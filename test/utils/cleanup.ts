d12a0f733ca99bb0d4f1664c57021f6dca4694da
Jonah Cadiz <cadizjo@oregonstate.edu>
2026-01-13 11:23:49 -0800

    fix request import error

--- test/utils/cleanup.ts
+++ test/utils/cleanup.ts
@@ -2,8 +2,11 @@ import { INestApplication } from '@nestjs/common';
 import { PrismaService } from '../../src/prisma/prisma.service';
 
 export async function cleanDb(app: INestApplication) {
+
+  // Get PrismaService instance
   const prisma = app.get(PrismaService);
 
+  // Delete data in proper order to avoid foreign key constraints
   await prisma.taskDependency.deleteMany();
   await prisma.task.deleteMany();
   await prisma.milestone.deleteMany();
