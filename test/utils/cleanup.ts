406189c14dd68291c8fed1d6a1a02e9123fd3459
Jonah Cadiz <cadizjo@oregonstate.edu>
2026-01-13 05:46:35 -0800

    fixing cleanDb util

--- test/utils/cleanup.ts
+++ test/utils/cleanup.ts
@@ -1,9 +1,12 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env.test' }); 
+
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-// Cleans up the database by deleting all tables used in tests
 export async function cleanDb() {
+  await prisma.taskDependency.deleteMany();
   await prisma.task.deleteMany();
   await prisma.milestone.deleteMany();
   await prisma.goal.deleteMany();
